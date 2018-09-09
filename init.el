3eec66762ec146220c57b1974e44f06cb3377eb0
Ivan Nečas <inecas@redhat.com>
2018-09-09 15:11:23 +0200

    Expose 'evil-jump-forward behind C-i

--- init.el
+++ init.el
@@ -325,6 +325,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (global-set-key "\C-i" 'evil-jump-forward) ;; otherwise, there is something else mapping to it
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
