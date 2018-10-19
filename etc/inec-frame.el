fd418ba81b6a233fd012a45b79f4dbb8df6620ef
Ivan Nečas <inecas@redhat.com>
2018-10-19 21:03:58 +0200

    Allow defining a project pointing to a file (vs. directory)

--- etc/inec-frame.el
+++ etc/inec-frame.el
@@ -24,30 +24,34 @@
        (select-frame-set-input-focus new-frame)
        t))))
 
-(defmacro inec-frame/-define-project (char name dir)
-  "map char to project name with dir"
+(defmacro inec-frame/-define-project (char name dir-or-file &optional file)
+  "map char to project name with dir-or-file"
   (let ((project-fun (intern (concat "inec-frame/project-" name))))
    `(progn
       (defun ,project-fun ()
         (interactive)
         (if (inec-frame/-open-my-project ,name)
             (progn
-              (magit-status ,dir)
+              (if ,file
+                  (find-file ,dir-or-file)
+                (magit-status ,dir-or-file))
               (delete-other-windows)
               )
-          ;  (projectile-switch-project-by-name ,dir t)
+          ;  (projectile-switch-project-by-name ,dir-or-file t)
           ))
       (spacemacs/set-leader-keys (concat "P" ,char) ',project-fun)
       )))
 
 (spacemacs/declare-prefix "P" "inec-project")
 (spacemacs/set-leader-keys "P'" 'inec-frame/new-custom)
+(inec-frame/-define-project "a" "apipie" "~/Projects/ws/apipie/apipie-rails")
 (inec-frame/-define-project "d" "dynflow" "~/Projects/ws/foreman-rex/dynflow")
 (inec-frame/-define-project "f" "foreman" "~/Projects/ws/foreman-rex/foreman-rails5")
 (inec-frame/-define-project "t" "foreman-tasks" "~/Projects/ws/foreman-rex/foreman-tasks")
 (inec-frame/-define-project "k" "katello" "~/Projects/ws/devel/katello")
 (inec-frame/-define-project "e" "emacs" "~/.spacemacs.d")
 (inec-frame/-define-project "s" "school" "~/Projects/ws/school/2018")
+(inec-frame/-define-project "o" "org" "~/Documents/org/todo_new.org" t)
 
 
 (provide 'inec-frame)
